292fec1d7c44dd516da08b644538f01a759e541b
fabricio <fabricxx@gmail.com>
2020-07-17 05:21:36 -0300

    adicionado a parte do codigo java

--- app/src/main/java/com/fabricio/android/MainActivity.java
+++ app/src/main/java/com/fabricio/android/MainActivity.java
@@ -1,16 +1,46 @@
 package com.fabricio.android;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.Random;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    String[] resposta = {"sim","Não conte com isso","Talvez"};
+    Random random = new Random();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
     }
+
+    //metodo criado para executar a ação do botão
+    public void acaoButton(View view) {
+
+        TextView helloTextView = findViewById(R.id.textView);
+        EditText palavra =  findViewById(R.id.TextPersonName);
+
+        if(palavra.getText().length()<=0){
+
+            helloTextView.setText("Faça a sua pergunta");
+
+        }else{
+            //receber o valor randomico
+            int index = random.nextInt(3);
+
+            helloTextView.setText(resposta[index]);
+
+            palavra.setText(null);
+        }
+    }
+
+
+
 }
